d9aa30bd719584acbf6ff9a3ef058381069731c8
baddapurisantosh <35330411+baddapurisantosh@users.noreply.github.com>
2020-06-08 09:01:22 +0100

    commting jenkins file

--- var/raamJenkinsfile.groovy
+++ var/raamJenkinsfile.groovy
@@ -1,1 +1,16 @@
-jnakak
+pipeline {
+    agent any
+	environment {
+	def buildCommand = "mvn clean install -DskipTests"
+	def mavenImage = "maven:3.6-jdk-8"
+	def registry = "baddapurisanthosh/myapps"
+    def registryCredential = ‘dockerhub’
+	}
+            stages {
+        stage ('checkout') {
+            steps {
+                git credentialsId: 'git', url: 'https://github.com/baddapurisantosh/raam.git'
+                }
+        }
+      }
+}
